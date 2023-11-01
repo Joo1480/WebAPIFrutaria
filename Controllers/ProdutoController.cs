198f34dd331cb3fa245e019526d1e3e492c24698
JOAO BERNARDELLI <joaobernardelli123@gmail.com>
2023-11-01 18:05:08 -0300

    criação da controller de produto

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIFrutaria.Models;
+using WebAPIFrutaria.Repositorios.Interfaces;
+
+namespace WebAPIFrutaria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly IProdutoRepositorio _produtoRepoitorio;
+
+        public ProdutoController(IProdutoRepositorio produtoRepoitorio)
+        {
+            _produtoRepoitorio = produtoRepoitorio;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<ProdutoModel>>> BuscarTodosProdutos()
+        {
+            List<ProdutoModel> produto = await _produtoRepoitorio.BuscarTodosProdutos();
+            return Ok(produto);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProdutoModel>> BuscarPorId(int id)
+        {
+            ProdutoModel produto = await _produtoRepoitorio.BuscarPorId(id);
+            return Ok(produto);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ProdutoModel>> Cadastrar([FromBody] ProdutoModel produtoModel)
+        {
+            ProdutoModel produto = await _produtoRepoitorio.Adicionar(produtoModel);
+            return Ok(produto);
+        }
+    }
+}
